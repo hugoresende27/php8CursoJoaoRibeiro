346f1f637e5862158e84de0401d2d8ed58355ef8
hugoresende27 <Hugoresende27@gmail.com>
2022-01-20 12:34:08 +0000

    a

--- aula26Prints.php
+++ aula26Prints.php
@@ -13,4 +13,8 @@ printf ("\n BINÁRIO:%b INTEIRO:%d CHAR:%c", $valor, $valor, $valor);
 $a = 100;
 $b = 999;
 //sprinf entre pelicas '' %num do argumento, $output // posso usar os argumentos mais do que uma vez
-echo sprintf (' aqui b %2$d e aqui a %1$d outra vez b em binário %2$-20b', $a, $b);
+echo sprintf (' aqui b %2$d e aqui a %1$d outra vez b em binário %2$b ', $a, $b);
+//capacidade : -------999
+echo sprintf ("capacidade : %'-10d", $b);
+//Dentro 999 está 00000100 valores
+echo sprintf ('Dentro %2$d está %1$08d valores', $a,$b);
