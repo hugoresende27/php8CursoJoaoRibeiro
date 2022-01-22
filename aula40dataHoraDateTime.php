56aef470370143f3a1598d57d674b32bc443935e
hugoresende27 <Hugoresende27@gmail.com>
2022-01-22 18:06:53 +0000

    a

--- aula40dataHoraDateTime.php
+++ aula40dataHoraDateTime.php
@@ -18,10 +18,52 @@ echo PHP_EOL;
 
 
 //////////////////////////// DATETIME //////////////////////////////////////////
+
 $agora = new DateTime();//agora é estático, vai ficar sempre com a hora em que é instanciado/definido
+//$agora = new DateTime('01-01-2000 23:12:30'); //posso instanciar agora com uma data já definida
+
 echo $agora->format('d-m-Y H:i:s');         //21-01-2022 22:32:58
 echo PHP_EOL;
 
+//setTime e setDate
+$agora ->setTime(23,23);        //setTime muda a hora
+$agora ->setDate(1999,3,9);
+echo $agora->format('d-m-Y H:i:s');         //09-03-1999 23:23:00
+echo PHP_EOL;
+
+//add ou sub DateInterval (cálcula anos bissextos)
+$agora->add(new DateInterval('P180D'));     //adiciona 180 dias
+$agora->add(new DateInterval('P12W'));     //adiciona 12 semanas
+$agora->add(new DateInterval('PT72H'));     //adiciona 72 horas
+$agora->sub(new DateInterval('PT7H'));     //subtrai 7 horas
+$agora->sub(new DateInterval('PT80H15M30S'));     //subtrai 7 horas 15 minutos e 30 segundos
+echo $agora->format('d-m-Y H:i:s');         
+echo PHP_EOL;
+
+////////////////////////////////// lista com datas ////////////////////////
+
+$datas = [
+    '21-10-1988',
+    '12-09-1996',
+    '01-07-2001',
+    '08-12-2005',
+    '22-11-1995'
+];
+
+foreach($datas as $d){
+    $info = DateTime::createFromFormat('d-m-Y', $d);    //instanciar cada data como um DateTime
+    $info->add(new DateInterval('P180D'));              //acrescenta 180 dias a cada data
+    echo $info->format('Y-m-d').PHP_EOL;
+}
+
+///////////////////////////////// diff ///////////////////////////////
+$data_divida = new DateTime('10-03-1997');
+$agora = new DateTime();
+
+$intervalo = $data_divida->diff($agora);   
+echo $intervalo->days; //print da diferença de dias 
+echo PHP_EOL;
+print_r($intervalo);
 
 
 ?>
